d7d69ac2dab9695247565951d7b99bdcf7485a36
Will Wong <williamwj.wong@gmail.com>
2022-02-24 16:53:08 -0800

    Clean up stack file

--- lib/nft_collection_cdk-stack.ts
+++ lib/nft_collection_cdk-stack.ts
@@ -1,13 +1,16 @@
+// CDK STACK PROPS
 import { Stack, StackProps } from 'aws-cdk-lib';
+
+// CONSTRUCT
 import { Construct } from 'constructs';
+
+// DYNAMO TABLE RESOURCE
 import { NFTCollectionDynamoTable } from './Resources/DynamoNFTCollection';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
 
 export class NftCollectionCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
 
     new NFTCollectionDynamoTable(this)
   }
